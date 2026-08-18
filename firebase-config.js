7d0793206abd2e0be4c8363f23717c07d5fbae26
Chegun15 <125580095+Chegun15@users.noreply.github.com>
2026-08-18 20:13:08 +0100

    Create firebase-config.js with Firebase setup
    Added Firebase configuration for the web app.

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+<script type="module">
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA0ZgNpNhMA5eVMcGFAJXsvgIQIZ1h6Pi4",
+    authDomain: "tontineapp-9f724.firebaseapp.com",
+    databaseURL: "https://tontineapp-9f724-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "tontineapp-9f724",
+    storageBucket: "tontineapp-9f724.firebasestorage.app",
+    messagingSenderId: "21158711906",
+    appId: "1:21158711906:web:4489eb84cfb320c7d9f73b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
