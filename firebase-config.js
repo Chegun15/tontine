4000f18d1054e1dd6f9f730f9f3cd004937b2f7e
Chegun15 <125580095+Chegun15@users.noreply.github.com>
2026-08-19 15:25:49 +0100

    Refactor Firebase config to use ES module syntax
    Refactor Firebase configuration to use ES module export syntax and remove unnecessary script tags.

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
-<script type="module">
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA0ZgNpNhMA5eVMcGFAJXsvgIQIZ1h6Pi4",
-    authDomain: "tontineapp-9f724.firebaseapp.com",
-    databaseURL: "https://tontineapp-9f724-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "tontineapp-9f724",
-    storageBucket: "tontineapp-9f724.firebasestorage.app",
-    messagingSenderId: "21158711906",
-    appId: "1:21158711906:web:4489eb84cfb320c7d9f73b"
-  };
+// Vos clés valides extraites de votre projet Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0ZgNpNhMA5eVMcGFAJXsvgIQIZ1h6Pi4",
+  authDomain: "tontineapp-9f724.firebaseapp.com",
+  databaseURL: "https://tontineapp-9f724-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tontineapp-9f724",
+  storageBucket: "tontineapp-9f724.firebasestorage.app",
+  messagingSenderId: "21158711906",
+  appId: "1:21158711906:web:4489eb84cfb320c7d9f73b"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Mettez votre clé Web3forms ici si vous souhaitez activer l'envoi d'e-mail automatique
+export const emailAccessKey = "00000000-0000-0000-0000-000000000000";
